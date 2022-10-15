437ed85ecffc108b3cd337a023f59da2a60901db
Jip Fr <jipfrijlink@gmail.com>
2022-10-15 20:00:23 +0200

    feat(dictionary): add custom format for dictionary, fix dictionary tooltip color inside links

--- assets/js/dictionary.js
+++ assets/js/dictionary.js
@@ -1,30 +1,40 @@
-const dictionary = {
-	Tzipora: {
-		text: "Also known as Zelda",
-	},
-	Vekllei: {
-		text: "This is a way longer piece of text bla bla bla This is a way longer piece of text bla bla bla This is a way longer piece of text bla bla bla ",
-	},
-};
+let dictionary = {};
 
-function handleDictionary() {
+function addDictionaryTooltips() {
 	document.querySelectorAll("p, li, blockquote").forEach((el) => {
+		
 		for (const phrase of Object.keys(dictionary)) {
-			const m = new RegExp(`(^|\\s+|\\(|>)(${phrase})(?=\\s+|$|\\)|<)`, "gi"); // Don't ask please
+			const m = new RegExp(`(^|\\s+|\\(|>)(${phrase})(?=\\s+|$|\\)|<|,)`, "gi"); // Don't ask please
+
 			if (el.innerHTML.match(m)) {
 				el.innerHTML = el.innerHTML.replace(
 					m,
-					`$1<dfn>$2${tooltip(dictionary[phrase].text)}</dfn>`
+					`$1<dfn>$2${tooltip(dictionary[phrase])}</dfn>`
 				);
 			}
 		}
+
 	});
 }
 
 function tooltip(text) {
 	return `<div class="tooltip ${
-		text.length > 100 ? "tooltip-long" : "tooltip-short"
+		text.length > 40 ? "tooltip-long" : "tooltip-short"
 	}">${text}</div>`;
 }
 
-window.addEventListener("load", handleDictionary);
+async function initDictionary() {
+	const dictionaryTxt = await fetch("/dictionary.txt").then((d) => d.text());
+	const dictArr = dictionaryTxt.split("\n").filter((t) => t && t.includes(":"));
+
+	for (const entry of dictArr) {
+		const [phrase, definition] = entry.split(":").map((v) => v.trim());
+		dictionary[phrase] = definition;
+	}
+
+	console.log(dictionary);
+
+	addDictionaryTooltips();
+}
+
+window.addEventListener("load", initDictionary);
